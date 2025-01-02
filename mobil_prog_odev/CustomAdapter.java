164d7e83da3c3569335ebe2db00504a77869ab85
Perihan Akdeniz <147817761+perihanakdnz@users.noreply.github.com>
2025-01-02 22:31:16 +0300

    CustomAdapter.java

--- mobil_prog_odev/CustomAdapter.java
+++ mobil_prog_odev/CustomAdapter.java
@@ -1,13 +1,52 @@
-public class CustomAdapter extends BaseAdapter {
-    private int[] imageArray;
-    private String[] nameArray;
-    private Context context;
+package com.example.mobil_prog_odev;
 
-    public CustomAdapter(int[] imageArray, String[] nameArray, Context context) {
-        this.imageArray = imageArray;
-        this.nameArray = nameArray;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class CustomAdapter extends BaseAdapter {
+    Context context;
+    String[] ad;
+    int[] resim;
+    LayoutInflater inflater;
+
+    public CustomAdapter(int[] resim, String[] ad, Context context) {
+        this.resim = resim;
+        this.ad = ad;
         this.context = context;
     }
 
-    // ... diğer metodlar aynı kalacak ...
-} 
+    @Override
+    public int getCount() {
+        return resim.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return resim[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View satir = LayoutInflater.from(context).inflate(R.layout.liste, parent, false);
+        ImageView img = satir.findViewById(R.id.imageView);
+        TextView txt = satir.findViewById(R.id.textView);
+
+        img.setBackgroundResource(resim[position]);
+        txt.setText(ad[position]);
+
+        return satir;
+    }
+}
