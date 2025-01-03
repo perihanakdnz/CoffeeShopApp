b73cde9f84a4b0ecc337666092eb085c01b6a654
Perihan Akdeniz <147817761+perihanakdnz@users.noreply.github.com>
2025-01-03 12:07:22 +0300

    NetworkCallback.kt

--- mobil_prog_odev/NetworkCallback.kt
+++ mobil_prog_odev/NetworkCallback.kt
@@ -2,12 +2,12 @@ class NetworkCallback(private val context: Context) {
     private val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     private val networkCallback = object : ConnectivityManager.NetworkCallback() {
         override fun onAvailable(network: Network) {
-            // İnternet bağlandığında
+        
             Toast.makeText(context, "İnternet bağlantısı kuruldu", Toast.LENGTH_SHORT).show()
         }
 
         override fun onLost(network: Network) {
-            // İnternet bağlantısı kesildiğinde
+        
             Toast.makeText(context, "İnternet bağlantısı kesildi", Toast.LENGTH_SHORT).show()
         }
     }
